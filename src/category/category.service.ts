7484a44dfa275a5d6a649efbbf6b9967d06fb9cb
Harsh Divecha <31266907+HarshDivecha@users.noreply.github.com>
2023-04-18 00:19:19 -0400

    simple log debug

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -19,7 +19,14 @@ export class CategoryService {
   }
 
   async getAllCategories(): Promise<Category[]> {
-    return this.prisma.category.findMany();
+    try{
+      return this.prisma.category.findMany();
+    }
+    catch(error){
+      console.log(error);
+      
+      return []
+    }
   }
 
   async getAllCategoriesWithMainItems(): Promise<Category[]> {
